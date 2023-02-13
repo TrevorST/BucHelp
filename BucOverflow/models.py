7b8e401757ac93dae3e494c832f8595fd431c855
Josiah Truelove <73202921+TrueloveJM@users.noreply.github.com>
2023-02-13 16:51:18 -0500

    Made Models for Dweets Tutorial in order to work on creating posts for website
    https://realpython.com/django-social-post-3/#step-8-create-the-back-end-logic-for-dweets
    
    underneath models.py
    had to change settings.py in the project configuration files

--- BucOverflow/models.py
+++ BucOverflow/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Dweet(models.Model):
+    user = models.ForeignKey(User, related_name="dweets", on_delete=models.DO_NOTHING)
+    body = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+class User(models.Model):
+    text = models.CharField(max_length=50)
+    
+
--- BucOverflow/views.py
+++ BucOverflow/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 def index(request):
     return HttpResponse("WELCOME TO BUCOVERFLOW")
