f6a2b2bb4112f2471e95ff23149433efa4fde9b3
Josiah Truelove <73202921+TrueloveJM@users.noreply.github.com>
2023-02-13 19:36:31 -0500

    Modified the Display Name of the Dweet Object in Admin page to reflect who wrote it and what time is was posted
    ^

--- BucOverflow/models.py
+++ BucOverflow/models.py
@@ -7,7 +7,15 @@ class Dweet(models.Model):
     user = models.ForeignKey(User, related_name="dweets", on_delete=models.DO_NOTHING)
     body = models.CharField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return (
+            f"{self.user} "
+            f"({self.created_at:%Y-%m-%d %H:%M}): "
+            f"{self.body[:30]}..."
+        )
 class User(models.Model):
     text = models.CharField(max_length=50)
+
     
 
