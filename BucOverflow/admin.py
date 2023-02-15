09fd1c3bb2f49997f1cdfeed933af43e04b0d1e5
Josiah Truelove <73202921+TrueloveJM@users.noreply.github.com>
2023-02-15 15:25:40 -0500

    Can't remember what I changed.

--- BucOverflow/admin.py
+++ BucOverflow/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Dweet
+from .models import Post
 
 # Register your models here.
 
-admin.site.register(Dweet)
+admin.site.register(Post)
--- BucOverflow/models.py
+++ BucOverflow/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Dweet(models.Model):
+class Post(models.Model):
     user = models.ForeignKey(User, related_name="dweets", on_delete=models.DO_NOTHING)
     body = models.CharField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
