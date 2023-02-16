88b556baa47f0cdfe4cfa2961d1ad308e6556710
Trevor <trevorst18@gmail.com>
2023-02-16 17:39:20 -0500

    Updated models.py

--- BucOverflow/models.py
+++ BucOverflow/models.py
@@ -4,12 +4,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
+    title = models.CharField(max_length=100)
+    #slug is the url
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
     user = models.ForeignKey(User, related_name="dweets", on_delete=models.DO_NOTHING)
-    body = models.CharField(max_length=140)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    comments = models.ManyToManyField(Comment, blank=True)
     
     def __str__(self):
         return (
+            f"{self.title}"
             f"{self.user} "
             f"({self.created_at:%Y-%m-%d %H:%M}): "
             f"{self.body[:30]}..."
@@ -17,5 +22,20 @@ class Post(models.Model):
 class User(models.Model):
     text = models.CharField(max_length=50)
 
+class Comment(models.Model):
+    user = models.ForeignKey(User, related_name="dweets", on_delete=models.DO_NOTHING)
+    content  = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    replies = models.ManyToManyField(Reply, blank=True)
+
+    def __str__(self):
+        return self.content[:100]
     
 
+class Reply(models.Model):
+    user = models.ForeignKey(User, related_name="dweets", on_delete=models.DO_NOTHING)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:100]
