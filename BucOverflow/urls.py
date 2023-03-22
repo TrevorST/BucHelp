26f698ed96f8efa3db04aadad23450a0d663e874
Reagan A Mullins <89862922+mullinsra@users.noreply.github.com>
2023-03-22 16:41:46 -0400

    hehehe

--- BucOverflow/urls.py
+++ BucOverflow/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
 
 from . import views
-from .views import base, index, discussions, home, SignUpView
+from .views import base, discussions, home, SignUpView
 
 app_name = "BucOverflow"
 
 urlpatterns = [
-    path("", views.base, name="base"),
-    path("index", views.index, name="index"),
+    path("", views.home, name="base"),
     path("discussions", views.discussions, name="discussions"),
     path("home", views.home, name="home"),
     path("signup", SignUpView.as_view(), name="signup"),
--- BucOverflow/views.py
+++ BucOverflow/views.py
@@ -28,9 +28,6 @@ from django.contrib.auth import logout as lt
 def base(request):
     return render(request, "base.html")
 
-def index(request):
-    return render(request, "index.html")
-
 def home(request):
     return render(request, "home.html")
 
